45069beb467b09e0ddc4b6dedf2e073cdc1092d9
Felipe Schossler <felipealvesschossler@gmail.com>
2019-03-07 00:13:02 -0300

    Finished until chapter 1, i create a example of CallStack treating a exception in your first caller

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -8,9 +8,37 @@ namespace ByteBank
 {
     class Program
     {
+        //Main Method
         static void Main(string[] args)
         {
+            try
+            {
+                Metodo();
+            }
+            catch(DivideByZeroException excecao)
+            {
+                Console.WriteLine(excecao.Message);
+                Console.WriteLine(excecao.StackTrace);
+            }
+
             Console.ReadLine();
         }
+        //end main method
+
+        static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        static void TestaDivisao(int divisor)
+        {
+            Dividir(10, divisor);
+        }
+
+        public static int Dividir(int numero, int divisor)
+        {
+            return numero / divisor;
+        }
+
     }
 }
